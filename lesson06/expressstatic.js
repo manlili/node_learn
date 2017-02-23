d1cbc46a37aba320952398c3aaaa19f0451e27dc
manlili <manlili@lechebang.com>
2017-02-23 16:04:06 +0800

    添加express静态文件

---
+++ lesson06/expressstatic.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static("public"));
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+var server = app.listen(4000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+})
